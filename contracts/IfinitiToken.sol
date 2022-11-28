665934a139867307e5ec020e94b67961f5e393b8
duhanov <maksim.duhanov@gmail.com>
2022-11-28 19:25:34 +0530

    init

--- contracts/IfinitiToken.sol
+++ contracts/IfinitiToken.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    constructor() ERC20("MyToken", "MTK") {}
+    constructor() ERC20("IfinitiToken", "IFT") {
+        _mint(msg.sender, 280000000 * 10 ** decimals());
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
